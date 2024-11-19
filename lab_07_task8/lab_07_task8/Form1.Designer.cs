e4908c74570e0c90275fe35435801e9a0a08c9ef
tooba26162 <233518@students.au.edu.pk>
2024-11-19 17:30:41 +0500

    Add files via upload

---
+++ lab_07_task8/lab_07_task8/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace lab_07_task8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblPhotoInfo = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(515, 194);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(236, 202);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblPhotoInfo
+            // 
+            this.lblPhotoInfo.AutoSize = true;
+            this.lblPhotoInfo.Location = new System.Drawing.Point(46, 207);
+            this.lblPhotoInfo.Name = "lblPhotoInfo";
+            this.lblPhotoInfo.Size = new System.Drawing.Size(35, 13);
+            this.lblPhotoInfo.TabIndex = 2;
+            this.lblPhotoInfo.Text = "label1";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(348, 202);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(458, 202);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 4;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblPhotoInfo);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblPhotoInfo;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+    }
+}
+
---
+++ lab_07_task8/lab_07_task8/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_07_task8
+{
+    public partial class Form1 : Form
+    {
+        private List<string> photoPaths; 
+        private int currentIndex;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            { if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    photoPaths = Directory.GetFiles(dialog.SelectedPath, "*.*").Where(file => file.ToLower().EndsWith("jpg") || file.ToLower().EndsWith("png") || file.ToLower().EndsWith("bmp")).ToList();
+                    if (photoPaths.Count > 0)
+                    { 
+                        currentIndex = 0;
+                        DisplayCurrentPhoto();
+                            
+                   }
+                    else
+                    {
+                        MessageBox.Show("No photos found in the selected folder.");
+                    }
+                } 
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (photoPaths.Count > 0)
+            {
+                currentIndex = (currentIndex + 1) % photoPaths.Count;
+                DisplayCurrentPhoto();
+            }
+        }
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (photoPaths.Count > 0)
+            {
+                currentIndex = (currentIndex - 1 + photoPaths.Count) % photoPaths.Count;
+                DisplayCurrentPhoto();
+            }
+        }
+        private void DisplayCurrentPhoto()
+        { 
+            if (photoPaths.Count > 0 && currentIndex >= 0) 
+            {
+                pictureBox1.Image = new Bitmap(photoPaths[currentIndex]);
+                lblPhotoInfo.Text = $"Photo {currentIndex + 1} of {photoPaths.Count}";
+            } 
+        }
+    }
+}
