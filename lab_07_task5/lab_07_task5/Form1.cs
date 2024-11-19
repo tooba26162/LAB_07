a197bfe221821c8a956df4843b3ba12e68eb7eb7
tooba26162 <233518@students.au.edu.pk>
2024-11-19 17:28:03 +0500

    Add files via upload

---
+++ lab_07_task5/lab_07_task5/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_07_task5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private long CalculateFactorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Factorial is not defined for negative numbers.");
+            }
+
+            long result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Parse the input number
+                int number = int.Parse(textBox1.Text);
+
+                // Calculate factorial using the function
+                long factorial = CalculateFactorial(number);
+
+                // Display the result
+                textBox2.Text = factorial.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid integer.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
