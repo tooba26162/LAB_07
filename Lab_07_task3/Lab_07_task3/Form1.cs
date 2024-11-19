7566c60db461d59375829af3d2d80d7b66a81c00
tooba26162 <233518@students.au.edu.pk>
2024-11-19 17:26:22 +0500

    Add files via upload

---
+++ Lab_07_task3/Lab_07_task3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_07_task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void CalculateSquares()
+        {
+            listBox1.Items.Clear(); // Clear the ListBox before adding new items
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int square = CalculateSquare(i);
+                listBox1.Items.Add($" {i}\t\t{square}");
+            }
+        }
+        private int CalculateSquare(int number)
+        {
+            return number * number;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalculateSquares();
+        }
+    }
+}
---
+++ Lab_07_task3/Lab_07_task3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab_07_task3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab_07_task3")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29023a17-0669-4ae1-b2b3-f7a471fe77bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
